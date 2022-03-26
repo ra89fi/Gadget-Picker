1753ed649c8f32ab299e157d3807c62fa901efd9
raihan <5421449+ra89fi@users.noreply.github.com>
2022-03-26 23:23:14 +0600

    q n a

--- src/App.js
+++ src/App.js
@@ -7,6 +7,27 @@ function App() {
         <div className="App">
             <Header></Header>
             <Shop></Shop>
+            <div>
+                <h3>How React works?</h3>
+                <p>
+                    React uses custom HTML elements in the form of JSX and a
+                    virtual DOM to keep track of elements rendered and when a
+                    change happens in the virtual DOM, React renders only that
+                    part in the actual DOM. That's why it is so performant.
+                </p>
+                <h3>What is the difference between state and props?</h3>
+                <p>
+                    State can change, props can't. An element can change its
+                    parents state using a passed in handler but it can never
+                    change the props it was passed on from its parent.
+                </p>
+                <h3>How useState works?</h3>
+                <p>
+                    It creates a state with passed in value as its
+                    initialization value and also returns function to change
+                    that state asynchronously.
+                </p>
+            </div>
         </div>
     );
 }
